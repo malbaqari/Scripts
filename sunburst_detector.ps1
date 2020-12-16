27254967f887c8681fd90d4e34867d6a3b3d8510
Mohammed Al Baqari <mohammed.albaqari@gmail.com>
2020-12-16 23:43:58 +0400

    Update sunburst_detector.ps1

--- sunburst_detector.ps1
+++ sunburst_detector.ps1
@@ -3,7 +3,7 @@ Foreach ($file in $Files)
 {
 	$size=[math]::floor($file.length / 100Mb)
 	if ($size -eq 0){
-		$hash = (get-filehash -path $file.fullname).hash
+		$hash = (get-filehash -path $file.fullname).hash 2>null
 		if ($hash){
 			if (gc .\hashes.txt | sls -pattern $hash){
 			echo $file.fullname

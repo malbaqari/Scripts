062ef3e080c90e704e679aa64db0c9e1898e0ba7
Mohammed Al Baqari <mohammed.albaqari@gmail.com>
2020-12-17 00:22:02 +0400

    Update sunburst_detector.ps1

--- sunburst_detector.ps1
+++ sunburst_detector.ps1
@@ -1,13 +1,57 @@
+write-output "Finding all files with size less than 100MB `n"
 $Files = ls -r \ 2>null
+
+$i = 0
 Foreach ($file in $Files)
 {
 	$size=[math]::floor($file.length / 100Mb)
 	if ($size -eq 0){
-		$hash = (get-filehash -path $file.fullname).hash 2>null
-		if ($hash){
-			if (gc .\hashes.txt | sls -pattern $hash){
-			echo $file.fullname
+		$i+=1
+	}
+}
+write-output ("Total Found "+ $i + " files `n")
+
+write-output ("Finding suspicious SHA256 Hashes `n")
+
+Foreach ($file in $Files)
+{
+	$size=[math]::floor($file.length / 100Mb)
+	if ($size -eq 0){
+		$sha2hash = (get-filehash -path $file.fullname -algorithm sha256).hash 2>null
+		if ($sha2hash){
+			if (gc .\hashes.txt | sls -pattern $sha2hash){
+			write-output ("Found Suspicious File at " + $file.fullname + "`n")
 			}
-	}	
+		}
+	}
+}
+
+write-output ("Finding suspicious SHA1 Hashes `n")
+
+Foreach ($file in $Files)
+{
+	$size=[math]::floor($file.length / 100Mb)
+	if ($size -eq 0){
+		$sha1hash = (get-filehash -path $file.fullname -algorithm sha1).hash 2>null
+		if ($sha1hash){
+			if (gc .\hashes.txt | sls -pattern $sha1hash){
+			write-output ("Found Suspicious File at " + $file.fullname + "`n")
+			}
+		}
+	}
+}
+
+write-output ("Finding suspicious MD5 Hashes `n")
+
+Foreach ($file in $Files)
+{
+	$size=[math]::floor($file.length / 100Mb)
+	if ($size -eq 0){
+		$md5hash = (get-filehash -path $file.fullname -algorithm md5).hash 2>null
+		if ($md5hash){
+			if (gc .\hashes.txt | sls -pattern $md5hash){
+			write-output ("Found Suspicious File at " + $file.fullname + "`n")
+			}
+		}
 	}
 }
